76a820dff8d16f8a449a8dde687ad5534167a791
Paco <paco@mastermind.ac>
2024-04-09 10:59:40 +0200

    Order and test

--- apirestful/api/main.py
+++ apirestful/api/main.py
@@ -1,17 +1,4 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine
-from sqlalchemy import text
-
-#Creacion del Engine contra la BBDD
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root@localhost/masterpodcast'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
-
-#Debug
-#comprobamos la correcta conexion
-#with engine.connect() as connection:
-#    result = connection.execute(text("select name from categories"))
-#    for row in result:
-#        print("nombre:", row.name)
 
 app = FastAPI()
 
