23a359f0241fac3a6602f3d1a048c83a3d447a10
Paco <paco@mastermind.ac>
2024-05-16 10:52:54 +0200

    Testing

--- apirestful/api/tests/test_categories.py
+++ apirestful/api/tests/test_categories.py
@@ -1,17 +1,42 @@
 from fastapi.testclient import TestClient
-from api import app
+from api import app,engine
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
 import pytest
 
 # Generamos el Test Cliente
 client = TestClient(app)
 
-
-# Generamos un fixture de datos
 @pytest.fixture
-def last_category():
-    return {}
+def db_init():
+    #Borra todos los datos de DB/Table Category
+    try:
+        with (engine.connect() as conn):
+            #Ahora mismo no hay relaciones pero cuando haya se deberá tener en cuenta para realizar el TRUNCATE
+            conn.execute(text("SET FOREIGN_KEY_CHECKS = 0;"))
+            conn.execute(text("TRUNCATE TABLE categories"))
+            conn.execute(text("SET FOREIGN_KEY_CHECKS = 1;"))
+            conn.execute(text("INSERT INTO categories (name) VALUES (:name)"),[
+                {"name": "music"},{"name": "technology"},{"name": "food"},{"name": "sports"},{"name": "art"}])
+            conn.commit()
+    except SQLAlchemyError as excinfo:
+            pytest.fail(f"Se ha producido un error en la conexion o en la consulta: {excinfo}")
 
 
+@pytest.fixture
+def db_init_cat_test():
+    try:
+        with (engine.connect() as conn):
+            #Ahora mismo no hay relaciones pero cuando haya se deberá tener en cuenta para realizar el TRUNCATE
+            conn.execute(text("SET FOREIGN_KEY_CHECKS = 0;"))
+            conn.execute(text("TRUNCATE TABLE categories"))
+            conn.execute(text("SET FOREIGN_KEY_CHECKS = 1;"))
+            conn.execute(text("INSERT INTO categories (name) VALUES (:name)"),[
+                {"name": "music"},{"name": "technology"},{"name": "food"},{"name": "sports"},{"name": "art"},{"name": "test"},
+            ])
+            conn.commit()
+    except SQLAlchemyError as excinfo:
+            pytest.fail(f"Se ha producido un error en la conexion o en la consulta: {excinfo}")
 # Test del endpoint GET /categories
 def test_read_categories():
     response = client.get("/categories/")
@@ -19,17 +44,28 @@ def test_read_categories():
     assert response.status_code == 200
     assert len(response.json()) > 0
 
+def test_read_second_category():
+    response = client.get("/categories/2")
+    assert response.status_code == 200
+    assert response.json() == {
+        "name": "technology",
+        "id": 2
+    }
 
-def test_insert_category(last_category):
+# Test del endpoint POST /categories
+def test_insert_category(db_init):
     response = client.post(
         "/categories/",
-        json={"name": "Foo Bar"},
+        json={"name": "Test Category"}
     )
     last_category = response.json()
-    print(last_category)
     assert response.status_code == 200
-    assert last_category['name'] == "Foo Bar"
+    assert response.json() == {
+        "name": "Test Category",
+        "id": 6
+    }
 
-def test_read_last_category(last_category):
-    print(last_category)
-    assert last_category['name'] == "Foo Bar"
+def test_delete_category(db_init_cat_test):
+    response = client.delete("/categories/6")
+    assert response.status_code == 200
+    assert len(response.json()) == 5
