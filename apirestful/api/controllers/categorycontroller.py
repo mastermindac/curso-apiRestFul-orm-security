ce39c4bfdde6adbdf527d27c662fb5fa54ac6486
Paco <paco@mastermind.ac>
2024-05-20 11:47:16 +0200

    Error control

--- apirestful/api/controllers/categorycontroller.py
+++ apirestful/api/controllers/categorycontroller.py
@@ -1,44 +1,66 @@
 from sqlalchemy import select
 from api import CategoryModel
 from api import CategoryCreateSchema
+from sqlalchemy.exc import NoResultFound, OperationalError
+from sqlalchemy.orm.exc import UnmappedInstanceError
+
 
 # Controller de categorias
 def get_categories(db):
-    #Crearia la consulta SELECT * from categories
-    stmt = select(CategoryModel)
-    #Lista de categorias
-    result = db.scalars(stmt)
-    categories=result.all()
+    try:
+        # Crearia la consulta SELECT * from categories
+        stmt = select(CategoryModel)
+        # Lista de categorias
+        result = db.scalars(stmt)
+        categories = result.all()
+    except OperationalError:
+        categories = None
     return categories
 
-def get_category(db, cat_id:int):
-    #Select del modelo ORM
-    categoryModel = db.execute(select(CategoryModel).filter_by(id=cat_id)).scalar_one()
+
+def get_category(db, cat_id: int):
+    try:
+        # Select del modelo ORM
+        result = db.execute(select(CategoryModel).filter_by(id=cat_id))
+        categoryModel = result.scalar_one()
+    except NoResultFound:
+        categoryModel = None
     return categoryModel
+
 
 def write_category(db, category: CategoryCreateSchema):
-    #Crearemos el modelo ORM a partir del Schema
+    # Crearemos el modelo ORM a partir del Schema
     categoryModel = CategoryModel(name=category.name)
-    #Insertamos en la DB
+    # Insertamos en la DB
     db.add(categoryModel)
-    #Commit
+    # Commit
     db.commit()
     db.refresh(categoryModel)
     return categoryModel
 
-def update_category(db, cat_id:int,category: CategoryCreateSchema):
-    #Select del modelo ORM
-    categoryModel = db.execute(select(CategoryModel).filter_by(id=cat_id)).scalar_one()
-    #Modificamos el name
-    categoryModel.name=category.name
-    #Commit
-    db.commit()
-    db.refresh(categoryModel)
+
+def update_category(db, cat_id: int, category: CategoryCreateSchema):
+    try:
+        # Select del modelo ORM
+        result = db.execute(select(CategoryModel).filter_by(id=cat_id))
+        categoryModel = result.scalar_one()
+        # Modificamos el name
+        categoryModel.name = category.name
+        # Commit
+        db.commit()
+        db.refresh(categoryModel)
+    except NoResultFound:
+        categoryModel = None
     return categoryModel
 
-def delete_category(db,cat_id:int,):
-    #Select del modelo ORM
-    category= db.get(CategoryModel,cat_id)
-    db.delete(category)
-    db.commit()
-    return get_categories(db)
+
+def delete_category(db, cat_id: int, ):
+    try:
+        # Select del modelo ORM
+        category = db.get(CategoryModel, cat_id)
+        db.delete(category)
+        db.commit()
+        categories = get_categories(db)
+    except UnmappedInstanceError:
+        categories = None
+    return categories
--- apirestful/api/routers/categories.py
+++ apirestful/api/routers/categories.py
@@ -1,30 +1,45 @@
-from fastapi import Depends , APIRouter
+from fastapi import Depends, APIRouter, HTTPException
 from sqlalchemy.orm import Session
-from api import categorycontroller,get_db
+from api import categorycontroller, get_db
 from api import CategorySchema, CategoryCreateSchema
 
-#Enrutador donde se definen los endpoints
+# Enrutador donde se definen los endpoints
 router = APIRouter()
+
+
 @router.get("/", response_model=list[CategorySchema])
 async def read_categories(db: Session = Depends(get_db)):
-    categories=categorycontroller.get_categories(db)
+    categories = categorycontroller.get_categories(db)
+    if categories == None:
+        raise HTTPException(status_code=503, detail="DB Unavailable")
     return categories
 
+
 @router.get("/{cat_id}", response_model=CategorySchema)
-async def read_category(cat_id:int,db: Session = Depends(get_db)):
-    category=categorycontroller.get_category(db,cat_id)
+async def read_category(cat_id: int, db: Session = Depends(get_db)):
+    category = categorycontroller.get_category(db, cat_id)
+    if category == None:
+        raise HTTPException(status_code=404, detail="Category not found")
     return category
 
+
 @router.post("/", response_model=CategorySchema)
-async def write_category(category:CategoryCreateSchema,db: Session = Depends(get_db)):
-    categoryResult=categorycontroller.write_category(db,category)
+async def write_category(category: CategoryCreateSchema, db: Session = Depends(get_db)):
+    categoryResult = categorycontroller.write_category(db, category)
     return categoryResult
+
 
 @router.put("/{cat_id}", response_model=CategorySchema)
-async def update_category(cat_id:int,category:CategoryCreateSchema,db: Session = Depends(get_db)):
-    categoryResult = categorycontroller.update_category(db, cat_id,category)
+async def update_category(cat_id: int, category: CategoryCreateSchema, db: Session = Depends(get_db)):
+    categoryResult = categorycontroller.update_category(db, cat_id, category)
+    if categoryResult == None:
+        raise HTTPException(status_code=404, detail="Category not found")
     return categoryResult
 
+
 @router.delete("/{cat_id}", response_model=list[CategorySchema])
-async def delete_category(cat_id:int, db: Session = Depends(get_db)):
-    return categorycontroller.delete_category(db, cat_id)
+async def delete_category(cat_id: int, db: Session = Depends(get_db)):
+    categories = categorycontroller.delete_category(db, cat_id)
+    if categories == None:
+        raise HTTPException(status_code=404, detail="Category not found")
+    return categories
